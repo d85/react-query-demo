7c599c80c976c50b6f8983c91952bcc819922139
Dan Carlier <dan.carlier@gmail.com>
2023-04-03 15:13:06 +1000

    Use react-query to get the superheroes, and then list their names

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,5 +1,25 @@
+import { useQuery } from 'react-query'
+import axios from 'axios'
+
+const fetchSuperHeroes = () => {
+  return axios.get('http://localhost:4000/superheroes')
+}
+
 export const RQSuperHeroesPage = () => {
+  const { isLoading, data } = useQuery('super-heroes', fetchSuperHeroes)
+
+  if (isLoading) {
+    return <h2>Loading...</h2>
+  }
+
   return (
-    <div>RQSuperHeroes Page</div>
+    <>
+      <h2>RQSuperHeroes Page</h2>
+      {
+        data?.data.map(hero => {
+          return <div key={hero.name}>{hero.name}</div>
+        })
+      }
+    </>
   )
 }
