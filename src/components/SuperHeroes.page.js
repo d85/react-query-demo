56cd915a1a28d666846e4f50583a255999af71be
Dan Carlier <dan.carlier@gmail.com>
2023-04-03 15:18:08 +1000

    Get superheroes, traditional error handling

--- src/components/SuperHeroes.page.js
+++ src/components/SuperHeroes.page.js
@@ -4,14 +4,23 @@ import axios from 'axios'
 export const SuperHeroesPage = () => {
   const [isLoading, setIsLoading] = useState(true)
   const [data, setData] = useState([])
+  const [error, setError] = useState('')
 
   useEffect(() => {
-    axios.get('http://localhost:4000/superheroes')
+    axios.get('http://localhost:4000/superheroes1')
       .then((res) => {
         setData(res.data)
         setIsLoading(false)
       })
+      .catch(error => {
+        setError(error.message)
+        setIsLoading(false)
+      })
   }, [])
+
+  if (error) {
+    return <h2>{error}</h2>
+  }
 
   if (isLoading) {
     return <h2>Loading...</h2>
