d6336ee09b0b5bdfb68d9b90593e3c804c8bdac8
Dan Carlier <dan.carlier@gmail.com>
2023-04-12 16:15:11 +1000

    (re)fetch data on button click

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -6,17 +6,16 @@ const fetchSuperHeroes = () => {
 }
 
 export const RQSuperHeroesPage = () => {
-  const { isLoading, data, isError, error, isFetching } = useQuery(
+  const { isLoading, data, isError, error, isFetching, refetch } = useQuery(
     'super-heroes', 
     fetchSuperHeroes, 
     {
-      refetchInterval: 2000,
-      refetchIntervalInBackground: true
+      enabled: false
     })
 
   console.log({ isLoading, isFetching })
 
-  if (isLoading) {
+  if (isLoading || isFetching) {
     return <h2>Loading...</h2>
   }
 
@@ -27,6 +26,7 @@ export const RQSuperHeroesPage = () => {
   return (
     <>
       <h2>RQSuperHeroes Page</h2>
+      <button onClick={refetch}>Fetch heroes</button>
       {
         data?.data.map(hero => {
           return <div key={hero.name}>{hero.name}</div>
