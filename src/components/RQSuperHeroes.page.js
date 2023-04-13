d199ceb4b7c01a59f459a10b1dd621bea26e5546
Dan Carlier <dan.carlier@gmail.com>
2023-04-13 18:32:51 +1000

    Add onSuccess and onError callbacks

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -6,11 +6,21 @@ const fetchSuperHeroes = () => {
 }
 
 export const RQSuperHeroesPage = () => {
+
+  const onSuccess = (data) => {
+    console.log('Perform side effect after data fetching', data)
+  }
+
+  const onError = (error) => {
+    console.log('Perform side effect after encountering error', error)
+  }
+
   const { isLoading, data, isError, error, isFetching, refetch } = useQuery(
     'super-heroes', 
     fetchSuperHeroes, 
     {
-      enabled: false
+      onSuccess,
+      onError
     })
 
   console.log({ isLoading, isFetching })
