6c771efa3a818d5854f624b672ab1b2574115099
Dan Carlier <dan.carlier@gmail.com>
2023-04-21 16:29:06 +1000

    Dynamic parallel queries using useQueries to get arbitrary number of hero results

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { QueryClientProvider, QueryClient } from 'react-query'
 import { ReactQueryDevtools } from 'react-query/devtools'
 import { HomePage } from './components/Home.page'
 import { ParallelQueriesPage } from './components/ParallelQueries.page'
+import { DynamicParallelPage } from './components/DynamicParallel.page'
 import { RQSuperHeroesPage } from './components/RQSuperHeroes.page'
 import { RQSuperHeroPage } from './components/RQSuperHero.page'
 import { SuperHeroesPage } from './components/SuperHeroes.page'
@@ -29,6 +30,7 @@ function App() {
           </ul>
         </nav>
         <Routes>
+          <Route path='rq-dynamic-parallel' element={<DynamicParallelPage heroIds={[1, 3]}/>} />
           <Route path='rq-parallel' element={<ParallelQueriesPage />} />
           <Route path='rq-super-heroes/:heroId' element={<RQSuperHeroPage />} />
           <Route path='/super-heroes' element={<SuperHeroesPage />} />
