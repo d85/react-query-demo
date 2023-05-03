6d44a0f6ab94b32ae193b083a920fff72eb4a94e
Dan Carlier <dan.carlier@gmail.com>
2023-05-03 19:07:52 +1000

    Refetch and update superhero list after 'add hero' click with query invalidation

--- src/hooks/useSuperHeroesData.js
+++ src/hooks/useSuperHeroesData.js
@@ -1,4 +1,4 @@
-import { useQuery, useMutation } from 'react-query'
+import { useQuery, useMutation, useQueryClient } from 'react-query'
 import axios from 'axios'
 
 const fetchSuperHeroes = () => {
@@ -24,5 +24,10 @@ export const useSuperHeroesData = (onSuccess, onError) => {
 }
 
 export const useAddSuperHeroData = () => {
-  return useMutation(addSuperHero)
+  const queryClient = useQueryClient()
+  return useMutation(addSuperHero, {
+    onSuccess: () => {
+      queryClient.invalidateQueries('super-heroes')
+    }
+  })
 }
